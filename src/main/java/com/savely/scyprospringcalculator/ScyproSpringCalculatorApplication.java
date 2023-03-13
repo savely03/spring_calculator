e5f0e58f2a1b7f42b6faf9c2be483a40d162de78
Savely <savely.domnikov@yandex.ru>
2023-03-13 22:21:12 +0300

    Initial commit

---
+++ src/main/java/com/savely/scyprospringcalculator/ScyproSpringCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.savely.scyprospringcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScyproSpringCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScyproSpringCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savely/scyprospringcalculator/ScyproSpringCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savely.scyprospringcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScyproSpringCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
